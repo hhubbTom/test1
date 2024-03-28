a85ba128cb35b7b39ee25039067bd20f501acaed
hhubbTom <1421695892@qq.com>
2024-03-28 15:46:47 +0800

    message 6:Functional test now checks we can input a to-do item

--- functional_test1.py
+++ functional_test1.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.common.by import By
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -11,8 +14,28 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         #张三
         self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do',self.browser.title),"browser title was:" + self.browser.title
+        #他注意到
+        self.assertIn('To-Do',self.browser.title)
+            #,"browser title was:" + self.browser.title
+        header_text=self.browser.find_element(By.TAG_NAME,'h1').text
+        self.assertIn('To-Do',header_text)
 
+        #应用有
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        #他在文本输入框
+        inputbox.send_keys('Buy flowers')
+        #他按了
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id list table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy flowers', [row.text for row in rows])
+#页面中
         self.fail('Finish the test!')
 if __name__== '__main__':
     unittest.main()

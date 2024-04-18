58243835aee6114def87f41295f0e60a024408e9
hhubbTom <1421695892@qq.com>
2024-04-18 15:14:15 +0800

    message 13: refactor sleep()

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,6 +4,9 @@ from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.common.by import By
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT=10
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
@@ -12,10 +15,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
          self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:  # (2)
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')  # (3)
+                rows = table.find_elements(By.TAG_NAME,'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return  # (4)
+            except(AssertionError,WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         #张三
@@ -38,18 +49,16 @@ class NewVisitorTest(LiveServerTestCase):
         # 待办事项表格中显示了“1: Buy flowers”
 
         inputbox.send_keys(Keys.ENTER)#（3）
-        time.sleep(2)
-        self.check_for_row_in_list_table('1: Buy flowers')
+        self.wait_for_row_in_list_table('1: Buy flowers')
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 他输入了“Give a gift to Lisi”
         inputbox = self.browser.find_element(By.ID,'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('1: Buy flowers')
-        self.check_for_row_in_list_table('2: Give a gift to Lisi')
+        self.wait_for_row_in_list_table('1: Buy flowers')
+        self.wait_for_row_in_list_table('2: Give a gift to Lisi')
         #张三想知道这个网站
         self.fail('Finish the test!')
 

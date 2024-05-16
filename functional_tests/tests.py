5850e1f483da43423c094f23c25bb8a2f3b7cd40
hhubbTom <1421695892@qq.com>
2024-05-16 16:56:48 +0800

    message 21: refactor templates to use a base template

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -97,8 +97,14 @@ class NewVisitorTest(LiveServerTestCase):
         # 访问首页
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
-        # 完美的居中
+        # 她完美的居中
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+        #他居中
         inputbox.send_keys('testing')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: testing')
